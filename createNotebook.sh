3eaf4916adaeed615a7c91f84e2ddc94f22a7c90
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-09-12 17:38:14 +1000

    replace notebook infrastructure
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

---
+++ createNotebook.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export SERVER_FULLNAME="${1:-testing.db.faims.edu.au}" # dev, alpha, testing, selenium
+cp notebook.json faims3-temp-notebook-migrator/
+cd faims3-temp-notebook-migrator
+echo "Load Geochem ${SERVER_FULLNAME}"
+#sed -e "s/##SPID##/${SPID}/g" 20220110-model1.json > fiptemp.json
+bash loadNotebook.sh $SERVER_FULLNAME "Campus Survey $(date)" "notebook.json" "campus_survey_demo"
+rm -f faims3-temp-notebook-migrator/notebook.json
---
+++ replaceNotebook.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export SERVER_FULLNAME="${1:-testing.db.faims.edu.au}" # dev, alpha, testing, selenium
+# export SPID="${2:-$SERVER_FULLNAME}"
+# cp 20220110-model1.json faims3-temp-notebook-migrator/testnotebook.json
+# cp 20220110-model1.json faims3-temp-notebook-migrator/
+# cd faims3-temp-notebook-migrator
+echo "Load FIPSoilData ${SERVER_FULLNAME}"
+#sed -e "s/##SPID##/${SPID}/g" 20220110-model1.json > fiptemp.json
+#python faims3-temp-notebook-migrator/loadNotebook.py $SERVER_FULLNAME "FIPSoilData Notebook (v0.1.0) for ${SPID}" "fiptemp.json"
+python3.10 faims3-temp-notebook-migrator/loadNotebook.py --project_key "campus_survey_demo" --notebook_json "notebook.json"  --env-file=secret.json
